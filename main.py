762fa529753a35ea694f00b9841f9f0765bd1cd2
JR-4B3 <rabe.fynn@startmail.com>
2025-06-24 21:02:08 +0200

    counting words of text

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(text)
+    num_words = count_words(text)
+    print(f"{num_words} words found in the document")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def count_words(text):
+    return len(text.split())
 
 main()
