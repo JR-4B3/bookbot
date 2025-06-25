d45cb156115e02c7b4ad787ee8125562bbed46d9
JR-4B3 <rabe.fynn@startmail.com>
2025-06-25 09:38:48 +0200

    Add book report with sorted letter frequency list

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from stats import get_num_words, get_num_chars
+import sys
+from stats import get_num_words, get_num_chars, sorted_list
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+    text = get_book_text(sys.argv[1])
     num_words = get_num_words(text)
     num_char = get_num_chars(text)
-    print(f"{num_words} words found in the document")
-    print(num_char)
+    list = sorted_list(num_char)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing Book found at {sys.argv[1]}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in list:
+        char = item["char"]
+        num = item["num"]
+        if char.isalpha():
+            print(f"{char}: {num}")
+    print("============= END ===============")            
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -2,11 +2,25 @@ def get_num_words(text):
     return len(text.split())
 
 def get_num_chars(text):
-    y = text.lower()
-    char_dict = {}
-    for char in y:
-        if char in char_dict:
-            char_dict[char] += 1
+    lower = text.lower()
+    count_dict = {}
+    for char in lower:
+        if char in count_dict:
+            count_dict[char] += 1
         else:
-            char_dict[char] = 1
-    return char_dict
+            count_dict[char] = 1
+
+    return count_dict
+
+def sorted_list(dict):
+    dict_list = []
+
+    for char, count in dict.items():
+        dict_list.append({"char": char, "num": count})
+
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+     
+
+def sort_on(items):
+    return items["num"]
